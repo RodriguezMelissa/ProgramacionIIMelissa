4e707206a29f5e1145947388214840270204c6bf
RodriguezMelissa <95558508+RodriguezMelissa@users.noreply.github.com>
2021-12-05 19:23:16 +1400

    Create BlueV.java

---
+++ views/BlueV.java
@@ -0,0 +1,24 @@
+package views;
+
+import java.awt.Frame;
+import java.awt.Color;
+
+public class BlueV extends Wframe {
+
+	public BlueV(Frame parentFrame) {
+		super(parentFrame);
+		initComponents();
+	}
+	
+	public void initComponents() {
+		setSize(400,400);
+		setBackground(Color.BLUE); 
+		setVisible(true);
+
+		
+	}
+	
+
+	
+
+}
